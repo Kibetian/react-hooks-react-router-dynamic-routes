429976309640a8be88c0665ee463cd6dd78b31d5
Kibetian <126007714+Kibetian@users.noreply.github.com>
2023-07-27 12:47:14 +0300

    react router dynamic

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,27 @@
+// import React from "react";
+// import { NavLink } from "react-router-dom";
+
+// function NavBar() {
+//   return (
+//     <div
+//       style={{
+//         borderBottom: "2px solid black",
+//         paddingBottom: "10px",
+//         marginBottom: "12px",
+//       }}
+//     >
+//       <NavLink style={{ marginRight: "10px" }} to="/">
+//         Home
+//       </NavLink>
+//       <NavLink style={{ marginRight: "10px" }} to="/movies">
+//         Movies
+//       </NavLink>
+//     </div>
+//   );
+// }
+
+// export default NavBar;
+
 import React from "react";
 import { NavLink } from "react-router-dom";
 
@@ -10,7 +34,7 @@ function NavBar() {
         marginBottom: "12px",
       }}
     >
-      <NavLink style={{ marginRight: "10px" }} to="/">
+      <NavLink style={{ marginRight: "10px" }} exact to="/">
         Home
       </NavLink>
       <NavLink style={{ marginRight: "10px" }} to="/movies">
--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
+// // import React from "react";
+// // import ReactDOM from "react-dom";
+// // import { BrowserRouter as Router } from "react-router-dom";
+// // import App from "./components/App";
+
+// // ReactDOM.render(
+// //   <Router>
+// //     <App />
+// //   </Router>,
+// //   document.getElementById("root")
+// // );
+// import React from "react";
+// import { Route, useRouteMatch } from "react-router-dom";
+// import MoviesList from "./MoviesList";
+// import MovieShow from "./MovieShow";
+
+// function MoviesPage({ movies }) {
+//   const match = useRouteMatch();
+
+//   return (
+//     <div>
+//       <MoviesList movies={movies} />
+//       <Route exact path={`${match.url}`}>
+//         <h3>Choose a movie from the list above</h3>
+//       </Route>
+//       <Route path={`${match.url}/:movieId`}>
+//         <MovieShow movies={movies} />
+//       </Route>
+//     </div>
+//   );
+// }
+
+// export default MoviesPage;
+
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -9,3 +43,4 @@ ReactDOM.render(
   </Router>,
   document.getElementById("root")
 );
+
